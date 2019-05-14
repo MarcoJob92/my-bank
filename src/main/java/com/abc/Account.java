201e46b892a9c30ac6a71d1b2b69f476066c2f3d
Marco Furone <marco.job92@gmail.com>
2019-05-14 20:49:33 +0100

    Update Account.java

--- src/main/java/com/abc/Account.java
+++ src/main/java/com/abc/Account.java
@@ -2,11 +2,15 @@ package com.abc;
 
 import static java.lang.Math.abs;
 
+import java.util.AbstractMap;
 import java.util.ArrayList;
 import java.util.Currency;
 import java.util.Date;
 import java.util.List;
 import java.util.Locale;
+import java.util.Map.Entry;
+import java.util.stream.Collectors;
+
 import consts.Constants;
 
 public class Account {
@@ -17,13 +21,8 @@ public class Account {
     private final Date creationDate = new Date();
     private double interestEarned = 0;
     private final Locale locale;		// Used for the currency
-    private static Integer counter = 0;	// Used to assign a unique ID to accounts
-   
-    /**
-     * @param accountType
-     * @param locale : (an example is Locale.UK)
-     * @throws IllegalArgumentException
-     */
+    private static Integer counter = 0;	        // Used to assign a unique ID to accounts
+    
     public Account(int accountType, Locale locale) throws IllegalArgumentException {
     	// This make sure that only valid objects are created
     	if (accountType != Constants.ACCOUNT_CHECKING_ID &&
@@ -38,23 +37,35 @@ public class Account {
     }
     
     public long getId() {
-		return id;
-	}
+	return id;
+    }
     public int getAccountType() {
         return accountType;
     }
     public List<Transaction> getTransactions() {
-		return transactions;
-	}
+	return transactions;
+    }
     public double getInterestEarned() {
     	return interestEarned;
+    }
+    
+    // returns Map.Entry containing the Account's Earned Interest and the Currency
+    public Entry<String, Double> getInterestAndCurrency() {	
+    	return new AbstractMap.SimpleEntry<String, Double>(
+    			getCurrencySymbol(), interestEarned);
     }
     public String getCurrencySymbol(){
     	return Currency.getInstance(locale).getSymbol(locale);
     }
 	
     // Utility Methods
-
+    
+    public double getTotalAmount() {
+    	return transactions.stream()
+    			.map(t -> t.getAmount())
+    			.reduce(0.0, (sum, a) -> sum + a);
+    }
+    
     public void deposit(double amount) throws IllegalArgumentException {
         if (amount <= 0) {
             throw new IllegalArgumentException(AMOUNT_ERROR_MEX);
@@ -63,20 +74,20 @@ public class Account {
         }
     }
     
-	public void withdraw(double amount) throws IllegalArgumentException, UnsupportedOperationException  {
-		// I want to assume that for this banking app the account amounts can't be less than 0.
-		if (amount > getTotalAmount()) {
-			throw new UnsupportedOperationException(
-					"The withdrawal amount is greater than total account amount.");
-		}
-	    if (amount <= 0) {
-	        throw new IllegalArgumentException(AMOUNT_ERROR_MEX);
-	    } else {
-	        transactions.add(new Transaction(-amount, Constants.WITHDRAWAL));
-	    }
+    public void withdraw(double amount) throws IllegalArgumentException, UnsupportedOperationException  {
+        // account amount can't be less than 0.
+	if (amount > getTotalAmount()) {
+	    throw new UnsupportedOperationException(
+			"The withdrawal amount is greater than total account amount.");
 	}
+        if (amount <= 0) {
+	    throw new IllegalArgumentException(AMOUNT_ERROR_MEX);
+        } else {
+	    transactions.add(new Transaction(-amount, Constants.WITHDRAWAL));
+        }
+    }
 	
-	// It accrues one-day interest rates. Ideally it should be called once a day.
+    // It accrues one-day interest rates. Ideally it should be called once a day.
     public void accrueInterests() {
         double amount = getTotalAmount();
         switch(accountType){
@@ -90,9 +101,6 @@ public class Account {
 	            	interestEarned += ( ((double) 1/365) + calculateDailyInterest((amount-1000), Constants.ZERO_TWO_PERCENT) );
 	            break;
 	        case Constants.ACCOUNT_MAXI_SAVINGS_ID:
-	        	// Changed Maxi-Savings accounts to have an interest rate of:
-	        	// - 5% assuming no withdrawals in the past 10 days 
-	        	// - otherwise 0.1%
 	        	Transaction lastWithdrawal = getLastWithdrawal();
 	        	if( lastWithdrawal==null || lastWithdrawal.isOlder10Days() ) {
 	        		interestEarned += calculateDailyInterest(amount, Constants.FIVE_PERCENT);
@@ -107,37 +115,23 @@ public class Account {
     	return (amount * percentage) / 365;
     }
     
-    public double getTotalAmount() {
-    	double amount = 0.0;
-        for (Transaction t: transactions)
-            amount += t.getAmount();
-        return amount;
-    }
-    
     public Transaction getLastWithdrawal() {
-    	int size = transactions.size();
-    	for (int i=size-1; i>0; i--) {
-    		if ( transactions.get(i).getTransactionType().equals(Constants.WITHDRAWAL) ) {
-    			return transactions.get(i);
-    		}
-    	}
-    	return null;
+    	return transactions.stream()
+    			.filter(t -> t.getTransactionType().equals(Constants.WITHDRAWAL))
+    			.reduce((x, last) -> last).orElse(null);
     }
     
-    // Get Statement Account with all transactions
+    // Account Statement with all the transactions
     public String getStatement() {
-    	// Get account name
-    	String s = "\n" + getAccountTypeName() + "\n";
-        // Transactions
-        for (Transaction t : transactions) {
-        	s += "\t" + t.getTransactionType() + " " + formatAmount(t.getAmount()) + "\n";
-        }
-        s += "Total: " + formatAmount(getTotalAmount()) + "\n";
-        return s;
+    	return transactions.stream()
+    				.map(t -> t.getTransactionType() + " " + formatAmount(t.getAmount()))
+    				.collect(Collectors.joining(
+    						"\n\t",
+    						"\n" + getAccountTypeName() + "\n\t",
+    						"\nTotal: " + formatAmount(getTotalAmount()) + "\n"));
     }
     
     public String getAccountInformation() {
-    	// Get account 
     	String s = "\n" + getAccountTypeName() + "\n";
         s += "ID: " + id + "\n";
         s += "Currency: " + getCurrencySymbol() + "\n";
@@ -146,7 +140,7 @@ public class Account {
         return s;
     }
     
-    // Format amount including the right currency
+    // Formats amount including the related currency
     private String formatAmount(double amount) {
     	String formattedStr = getCurrencySymbol();
     	formattedStr += String.format("%,.2f", abs(amount));
